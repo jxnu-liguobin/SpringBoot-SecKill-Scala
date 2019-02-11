a47d91843595dd3da5acdc64ddd61d5ae35533dd
jxnu-liguobin <568845948@qq.com>
2019-02-11 22:24:27 +0800

    修改readme

--- SpringBoot-SecKill-Scala/src/main/scala/cn/edu/jxnu/seckill/SpringBootSecKillScalaApplication.scala
+++
@@ -1,8 +0,0 @@
-package cn.edu.jxnu.seckill
-
-import org.springframework.boot.SpringApplication
-
-object SpringBootSecKillScalaApplication extends App {
-    //classof相当于getClass/.class
-    SpringApplication.run(classOf[AppConfig])
-}
